3c6caac761c6be876ed5fac1b5d5d58f77eec3ce
Ajaya-magar <156473678+Ajaya-magar@users.noreply.github.com>
2024-04-18 15:35:04 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "raccoon"];
+let extension=[".com",".pt",".be",".cat"];
+let Output=[];
+
+function Name(value1, value2, value3, value4) {
+    for (let i = 0; i < value1.length; i++) {
+        for (let j = 0; j < value2.length; j++) {
+            for (let k = 0; k < value3.length; k++) {
+                for (let l = 0; l < value4.length; l++) {
+                    let domainName = value1[i] + value2[j] + value3[k] + value4[l];
+                    Output.push(domainName);
+                    let myTarget = document.getElementById('hello');
+                    let myElement = document.createElement('p');
+                    myElement.innerText = domainName;
+                    myTarget.appendChild(myElement);
+                }  
+            }
+        }
+    }
+}
+
+Name(pronoun, noun, adj, extension);
+console.log(Output);
+
+//
+//
+//
+ //
