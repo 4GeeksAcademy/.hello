6e0e807e25980f7f7fbf6b5950f4796824bb6e08
Ajaya-magar <156473678+Ajaya-magar@users.noreply.github.com>
2024-05-03 17:07:38 +0000

    First commit

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "raccoon"];
-let extension=[".com",".pt",".be",".cat"];
-let Output=[];
+const cardValue=['A','2','3','4','5','6','7','8','9','10','j','Q','k'];
+const cardSuits=['♠',' ♦','   ♣','   ♥'];
+4
+function randomcard(){
+ 
 
-function Name(value1, value2, value3, value4) {
-    for (let i = 0; i < value1.length; i++) {
-        for (let j = 0; j < value2.length; j++) {
-            for (let k = 0; k < value3.length; k++) {
-                for (let l = 0; l < value4.length; l++) {
-                    let domainName = value1[i] + value2[j] + value3[k] + value4[l];
-                    Output.push(domainName);
-                    let myTarget = document.getElementById('hello');
-                    let myElement = document.createElement('p');
-                    myElement.innerText = domainName;
-                    myTarget.appendChild(myElement);
-                }  
-            }
-        }
-    }
+ 
+ 
+
+ 
+
+
+
+
+
+    const randomValueIndex = Math.floor(Math.random() * cardValue.length);
+    const randomSuitIndex = Math.floor(Math.random() * cardSuits.length);
+    
+
+    const randomValue = cardValue[randomValueIndex];
+    const randomSuit = cardSuits[randomSuitIndex];
+
+
+
+    document.getElementById('card-first').innerText = randomSuit;
+    document.getElementById('card-second').innerText = randomValue;
+    document.getElementById('card-third').innerText=randomSuit;
+
 }
-
-Name(pronoun, noun, adj, extension);
-console.log(Output);
-
-//
-//
-//
- //
+randomcard()
+setInterval(randomcard, 10000);
